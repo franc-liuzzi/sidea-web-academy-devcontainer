72c74dab14c2bdd551222657f5dda9785df2cfed
Francesco Liuzzi <francesco.liuzzi@sideagroup.com>
2022-03-19 13:24:22 +0100

    add devcointainer-update as global command

--- Makefile
+++ Makefile
@@ -34,6 +34,7 @@ release-zip:
 	zip -r sidea-workspace.zip .devcontainer/
 	git add .
 	git commit -m release $(shell composer get:version)
+	git push
 
 stack-up:
 	docker-compose \
--- devcontainer/app/self-update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /workspace
-
-wget --quiet "https://github.com/franc-liuzzi/sidea-web-academy-devcontainer/blob/master/sidea-workspace.zip?raw=true" sidea-workspace.zip
-unzip -u sidea-workspace.zip
