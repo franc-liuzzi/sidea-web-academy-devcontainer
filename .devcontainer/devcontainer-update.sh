02f2267ea10c2eff31c688b6c9246b25b330576e
Francesco Liuzzi <francesco.liuzzi@sideagroup.com>
2022-03-19 13:01:48 +0100

    release 1.1.1: fix exercism, add devcointainer-update

---
+++ .devcontainer/devcontainer-update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget --quiet -O /tmp/sidea-workspace.zip "https://github.com/franc-liuzzi/sidea-web-academy-devcontainer/blob/master/sidea-workspace.zip?raw=true"
+unzip -o /tmp/sidea-workspace.zip -d /workspace
---
+++ devcontainer/app/self-update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /workspace
+
+wget --quiet "https://github.com/franc-liuzzi/sidea-web-academy-devcontainer/blob/master/sidea-workspace.zip?raw=true" sidea-workspace.zip
+unzip -u sidea-workspace.zip
