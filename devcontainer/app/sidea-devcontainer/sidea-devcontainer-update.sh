2442ba2d131bb00a9617915a704b75f1c1a2ab6f
Francesco Liuzzi <francesco.liuzzi@sideagroup.com>
2022-03-20 21:48:49 +0100

    adding version check before update

--- devcontainer/app/sidea-devcontainer/sidea-devcontainer-update.sh
+++ devcontainer/app/sidea-devcontainer/sidea-devcontainer-update.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-wget --quiet -O /tmp/sidea-workspace.zip "https://github.com/franc-liuzzi/sidea-web-academy-devcontainer/blob/master/sidea-workspace.zip?raw=true"
-rm /workspace/.devcontainer/*
+if [ "$(cat .devcontainer/sidea-devcontainer.version)" == \
+    "$(curl -s https://raw.githubusercontent.com/franc-liuzzi/sidea-web-academy-devcontainer/master/.devcontainer/sidea-devcontainer.version)" ]; then
+    echo "You're already using the latest version";
+    exit 0
+fi
+
+wget --quiet -O /tmp/sidea-workspace.zip "https://raw.githubusercontent.com/franc-liuzzi/sidea-web-academy-devcontainer/master/sidea-workspace.zip"
+rm -rf /workspace/.devcontainer/* /workspace/.vscode/*
 unzip -o /tmp/sidea-workspace.zip -d /workspace
 echo ""
 echo "Devcontainer updated. Now you can restart your dev environment."
